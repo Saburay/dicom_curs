a40041b906e11dbc9ca1a99eb0f9bfd02d1b776e
saburay32 <saburay32@gmail.com>
2024-02-19 19:27:31 +0300

    2 commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# https://startdatajourney.com/ru/course/medical-image-analysis-in-python/modules/20/69/1
+# Курс Анализ медицинских изображений в Python  теперь бесплатный для всех.
+#
+# На курсе вы изучите анализ медицинских изображений с помощью Python.
+# Вы будете изучать КТ и рентген снимки, сегментировать области изображения и проводить анализ метаданных.
+# Даже если вы никогда раньше не работали с медицинскими изображениями, то по завершению курса вы будете обладать всеми необходимы навыками.
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
